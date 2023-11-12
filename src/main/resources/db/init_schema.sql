923d2b0d297d6153c6b35822a3921f137caf06b4
신명상 <mongddangand@likelion.org>
2023-11-12 23:17:52 +0900

    jpa_local 정책 변경

---
+++ src/main/resources/db/init_schema.sql
@@ -0,0 +1,7 @@
+create table users
+(
+    id       bigint primary key comment '유저 시퀀스',
+    email    varchar(50)  not null comment '이메일',
+    password varchar(255) not null comment '비밀번호',
+    unique uk_email(email)
+);
