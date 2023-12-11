4401fc6eb6bc5b6d93c30689f49b618ee657624c
신명상 <mongddangand@likelion.org>
2023-12-11 22:19:38 +0900

    Spring security 권한 수정

--- src/main/java/com/sms/me/realworld/api/controller/UserController.java
+++ src/main/java/com/sms/me/realworld/api/controller/UserController.java
@@ -2,14 +2,11 @@ package com.sms.me.realworld.api.controller;
 
 import com.sms.me.realworld.api.controller.dto.SignupRequest;
 import com.sms.me.realworld.api.controller.dto.SignupResponse;
-import com.sms.me.realworld.api.security.AuthUserDetails;
 import com.sms.me.realworld.core.domain.user.User;
 import com.sms.me.realworld.core.domain.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +21,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    @PreAuthorize("hasRole('USER')")
     public SignupResponse signup(
-            @RequestBody @Valid SignupRequest request,
-            @AuthenticationPrincipal AuthUserDetails userDetails
+            @RequestBody @Valid SignupRequest request
     ) {
-        log.info("id: {}, role: {}", userDetails.userId, userDetails.roleType);
         User user = userService.signup(request.toCommand());
         return SignupResponse.of(user);
     }
